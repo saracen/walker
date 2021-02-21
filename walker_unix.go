81a0208b97a0c7805bd723ca4463b827a1312edf
Ichinose Shogo <shogo82148@gmail.com>
2021-02-22 02:03:18 +0900

    fix "interrupted system call" error
    According to https://golang.org/doc/go1.14#runtime
    A consequence of the implementation of preemption is that on Unix systems, including Linux and macOS
    systems, programs built with Go 1.14 will receive more signals than programs built with earlier releases.
    
    This causes syscall.Open and syscall.ReadDirent sometimes fail with EINTR errors.
    We need to retry in this case.

--- walker_unix.go
+++ walker_unix.go
@@ -9,7 +9,7 @@ import (
 )
 
 func (w *walker) readdir(dirname string) error {
-	fd, err := syscall.Open(dirname, 0, 0)
+	fd, err := open(dirname, 0, 0)
 	if err != nil {
 		return &os.PathError{Op: "open", Path: dirname, Err: err}
 	}
@@ -23,7 +23,7 @@ func (w *walker) readdir(dirname string) error {
 	for {
 		if bufp >= nbuf {
 			bufp = 0
-			nbuf, err = syscall.ReadDirent(fd, buf)
+			nbuf, err = readDirent(fd, buf)
 			if err != nil {
 				return err
 			}
@@ -48,5 +48,40 @@ func (w *walker) readdir(dirname string) error {
 			}
 		}
 	}
-	return nil
+	// never reach
+}
+
+// According to https://golang.org/doc/go1.14#runtime
+// A consequence of the implementation of preemption is that on Unix systems, including Linux and macOS
+// systems, programs built with Go 1.14 will receive more signals than programs built with earlier releases.
+//
+// This causes syscall.Open and syscall.ReadDirent sometimes fail with EINTR errors.
+// We need to retry in this case.
+type temporaryError interface {
+	error
+	Temporary() bool
+}
+
+func open(path string, mode int, perm uint32) (fd int, err error) {
+	for {
+		nbuf, err := syscall.Open(path, mode, perm)
+		if err == nil {
+			return nbuf, nil
+		}
+		if err, ok := err.(temporaryError); !ok || !err.Temporary() {
+			return 0, err
+		}
+	}
+}
+
+func readDirent(fd int, buf []byte) (n int, err error) {
+	for {
+		nbuf, err := syscall.ReadDirent(fd, buf)
+		if err == nil {
+			return nbuf, nil
+		}
+		if err, ok := err.(temporaryError); !ok || !err.Temporary() {
+			return 0, err
+		}
+	}
 }
